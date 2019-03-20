048f1f46a4103515112541f98ac0c9cf954ff728
twiliHarsh <41773164+twiliHarsh@users.noreply.github.com>
2019-03-20 23:29:14 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,81 +1,107 @@
-// This sample shows how to use [onDeleted] to remove an entry when the
-// delete button is tapped.
-
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title:
-          'Flutter Code Sample for material.DeletableChipAttributes.onDeleted',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: MyStatefulWidget(),
+    return new MaterialApp(
+      theme: new ThemeData(primarySwatch: Colors.blue),
+      home: new LoginPage(),
     );
   }
 }
 
-class Actor {
-  const Actor(this.name, this.initials);
-  final String name;
-  final String initials;
-}
-
-class CastList extends StatefulWidget {
+class LoginPage extends StatefulWidget {
   @override
-  State createState() => CastListState();
+  State createState() => new LoginPageState();
 }
 
-class CastListState extends State<CastList> {
-  final List<Actor> _cast = <Actor>[
-    const Actor('Aaron Burr', 'AB'),
-    const Actor('Alexander Hamilton', 'AH'),
-    const Actor('Eliza Hamilton', 'EH'),
-    const Actor('James Madison', 'JM'),
-  ];
+class LoginPageState extends State<LoginPage>
+    with SingleTickerProviderStateMixin {
+  Animation<double> _iconAnimation;
+  AnimationController _iconAnimationController;
 
-  Iterable<Widget> get actorWidgets sync* {
-    for (Actor actor in _cast) {
-      yield Padding(
-        padding: const EdgeInsets.all(4.0),
-        child: Chip(
-          avatar: CircleAvatar(child: Text(actor.initials)),
-          label: Text(actor.name),
-          onDeleted: () {
-            setState(() {
-              _cast.removeWhere((Actor entry) {
-                return entry.name == actor.name;
-              });
-            });
-          },
+  @override
+  void initState() {
+    super.initState();
+    _iconAnimationController = new AnimationController(
+        vsync: this, duration: new Duration(milliseconds: 500));
+    _iconAnimation = new CurvedAnimation(
+      parent: _iconAnimationController,
+      curve: Curves.bounceOut,
+    );
+    _iconAnimation.addListener(() => this.setState(() {}));
+    _iconAnimationController.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    return new Scaffold(
+      backgroundColor: Colors.white,
+      body: new Stack(fit: StackFit.expand, children: <Widget>[
+        new Image(
+          image: new AssetImage("assets/girl.jpeg"),
+          fit: BoxFit.cover,
+          colorBlendMode: BlendMode.darken,
+          color: Colors.black87,
         ),
-      );
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Wrap(
-      children: actorWidgets.toList(),
+        new Theme(
+          data: new ThemeData(
+              brightness: Brightness.dark,
+              inputDecorationTheme: new InputDecorationTheme(
+                // hintStyle: new TextStyle(color: Colors.blue, fontSize: 20.0),
+                labelStyle:
+                    new TextStyle(color: Colors.tealAccent, fontSize: 25.0),
+              )),
+          isMaterialAppTheme: true,
+          child: new Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              new FlutterLogo(
+                size: _iconAnimation.value * 140.0,
+              ),
+              new Container(
+                padding: const EdgeInsets.all(40.0),
+                child: new Form(
+                  autovalidate: true,
+                  child: new Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      new TextFormField(
+                        decoration: new InputDecoration(
+                            labelText: "Enter Email", fillColor: Colors.white),
+                        keyboardType: TextInputType.emailAddress,
+                      ),
+                      new TextFormField(
+                        decoration: new InputDecoration(
+                          labelText: "Enter Password",
+                        ),
+                        obscureText: true,
+                        keyboardType: TextInputType.text,
+                      ),
+                      new Padding(
+                        padding: const EdgeInsets.only(top: 60.0),
+                      ),
+                      new MaterialButton(
+                        height: 50.0,
+                        minWidth: 150.0,
+                        color: Colors.green,
+                        splashColor: Colors.teal,
+                        textColor: Colors.white,
+                        child: new Icon(FontAwesomeIcons.signInAlt),
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ]),
     );
-  }
-}
-
-class MyStatefulWidget extends StatefulWidget {
-  MyStatefulWidget({Key key}) : super(key: key);
-
-  @override
-  _MyStatefulWidgetState createState() => _MyStatefulWidgetState();
-}
-
-class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  @override
-  Widget build(BuildContext context) {
-    return CastList();
   }
 }
